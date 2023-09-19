30969807d9adde81a2d3c541cc6a9c0bd2325815
canonical <canonical_entropy@163.com>
2023-09-19 20:50:57 +0800

    禁用不使用的单元测试

--- nop-rule/nop-rule-service/src/test/java/io/nop/rule/service/TestRuleService.java
+++ nop-rule/nop-rule-service/src/test/java/io/nop/rule/service/TestRuleService.java
@@ -5,7 +5,9 @@ import io.nop.graphql.core.engine.IGraphQLEngine;
 import io.nop.rule.api.RuleService;
 
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 public class TestRuleService extends JunitAutoTestCase {
     @Inject
     IGraphQLEngine graphQLEngine;
