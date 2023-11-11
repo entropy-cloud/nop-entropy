1a0cb2d0c4fdbf2965c21d381cabc3f6d206987e
canonical <canonical_entropy@163.com>
2023-11-11 18:09:16 +0800

    更新nop-chaos代码，使用sdk方式引入nop-chaos核心代码

---
+++ nop-web-site/src/main/resources/META-INF/resources/nop/app-starter.js
@@ -0,0 +1,47 @@
+System.register(['@nop-chaos/sdk','vue','react'], (function (exports) {
+	'use strict';
+	var sdk, Vue, React;
+	return {
+		setters: [module => {
+			sdk = module;
+		}, module => {
+			Vue = module;
+		}, module =>{
+		    React = module
+		}],
+		execute: (function () {
+			const componentType = sdk.defineReactPageComponent(()=>{
+				return {
+				  getComponent(name) {
+						
+				  },
+			  
+				  getScopedStore(name) {
+					
+				  },
+			  
+				  getState(name) {
+					
+				  },
+			  
+				  setState(name, value) {
+					
+				  },
+			  
+				  onDestroyPage(page) {
+					
+				  },
+			  
+				  async onRenderPage(schema, data, page) {
+					return React.createElement('div',null, JSON.stringify(schema))
+				  }
+				}
+			})
+
+			sdk.registerSchemaType('plain',{
+				componentType,
+				editorComponentType: componentType
+			})
+		})
+	};
+}));
