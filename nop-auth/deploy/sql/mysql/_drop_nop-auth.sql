979b5002aa2af187cc02a1b3691eab26bff203c0
canonical <canonical_entropy@163.com>
2023-11-26 20:37:28 +0800

    用户分组增加ownerId字段

--- nop-auth/deploy/sql/mysql/_drop_nop-auth.sql
+++ nop-auth/deploy/sql/mysql/_drop_nop-auth.sql
@@ -10,9 +10,9 @@ drop table  if exists  nop_auth_tenant;
 drop table  if exists  nop_auth_resource;
 drop table  if exists  nop_auth_role;
 drop table  if exists  nop_auth_session;
-drop table  if exists  nop_auth_user;
 drop table  if exists  nop_auth_group;
 drop table  if exists  nop_auth_site;
+drop table  if exists  nop_auth_user;
 drop table  if exists  nop_auth_dept;
 drop table  if exists  nop_auth_position;
 
