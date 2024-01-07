e8d6808284e6964cccd96354af5d39a385a54b73
canonical <canonical_entropy@163.com>
2024-01-07 10:17:25 +0800

    提交生成的SimsCollegeEx类

---
+++ nop-orm/src/test/java/io/nop/app/SimsCollegeEx.java
@@ -0,0 +1,11 @@
+package io.nop.app;
+
+import io.nop.api.core.annotations.biz.BizObjName;
+import io.nop.app._gen._SimsCollegeEx;
+
+
+@BizObjName("SimsCollegeEx")
+public class SimsCollegeEx extends _SimsCollegeEx{
+
+
+}
