650de410b407424ff76b343aafe2cd74116fbc87
canonical <canonical_entropy@163.com>
2024-06-12 20:15:03 +0800

    增加jdk17打包测试

---
+++ nop-demo/nop-jdk17-demo/src/main/java/io/nop/demo/Jdk17DemoMain.java
@@ -0,0 +1,11 @@
+package io.nop.demo;
+
+
+public class Jdk17DemoMain {
+    public static void main(String[] args) {
+        String str = """
+                Hello!
+                """;
+        System.out.println(str);
+    }
+}
