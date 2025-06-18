2444e6ba905338c677926ab2d2d5b617882e2c82
flytreeleft <flytreeleft@crazydan.org>
2025-06-18 09:05:35 +0800

    nop-idea-pugin: 使用 Java 15+ 的文本块语法编写多行文本

--- nop-idea-plugin/src/test/java/io/nop/idea/plugin/doc/TestXLangDocumentationProvider.java
+++ nop-idea-plugin/src/test/java/io/nop/idea/plugin/doc/TestXLangDocumentationProvider.java
@@ -6,6 +6,8 @@ import com.intellij.psi.PsiElement;
 import io.nop.idea.plugin.BaseXLangPluginTestCase;
 
 /**
+ * 参考 https://github.com/JetBrains/intellij-community/blob/master/xml/tests/src/com/intellij/html/HtmlDocumentationTest.java
+ *
  * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
  * @date 2025-06-17
  */
@@ -18,23 +20,31 @@ public class TestXLangDocumentationProvider extends BaseXLangPluginTestCase {
     }
 
     public void testGenerateDocForXmlName() {
-        doTest("<exam<caret>ple xmlns:x=\"/nop/schema/xdsl.xdef\" x:schema=\"/test/doc/example.xdef\">"
-               + "    <child name=\"Child\"/>"
-               + "</example>", "<p>This is root node</p>\n");
+        doTest("""
+                       <exam<caret>ple xmlns:x="/nop/schema/xdsl.xdef" x:schema="/test/doc/example.xdef">
+                           <child name="Child"/>
+                       </example>
+                       """, "<p>This is root node</p>\n");
 
-        doTest("<example xmlns:x=\"/nop/schema/xdsl.xdef\" x:schema=\"/test/doc/example.xdef\">"
-               + "    <ch<caret>ild name=\"Child\"/>"
-               + "</example>", "<p>This is child node</p>\n");
+        doTest("""
+                       <example xmlns:x="/nop/schema/xdsl.xdef" x:schema="/test/doc/example.xdef">
+                           <ch<caret>ild name="Child"/>
+                       </example>
+                       """, "<p>This is child node</p>\n");
 
-        doTest("<example xmlns:x=\"/nop/schema/xdsl.xdef\" x:schema=\"/test/doc/example.xdef\">"
-               + "    <child na<caret>me=\"Child\"/>"
-               + "</example>", "<p>stdDomain=string</p><hr/><br/><p>This is child name</p>\n");
+        doTest("""
+                       <example xmlns:x="/nop/schema/xdsl.xdef" x:schema="/test/doc/example.xdef">
+                           <child na<caret>me="Child"/>
+                       </example>
+                       """, "<p>stdDomain=string</p><hr/><br/><p>This is child name</p>\n");
     }
 
     public void testGenerateDocForXmlAttributeValue() {
-        doTest("<example xmlns:x=\"/nop/schema/xdsl.xdef\" x:schema=\"/test/doc/example.xdef\">"
-               + "    <child type=\"le<caret>af\"/>"
-               + "</example>", "<p><b>leaf-Leaf Node</b></p>");
+        doTest("""
+                       <example xmlns:x="/nop/schema/xdsl.xdef" x:schema="/test/doc/example.xdef">
+                           <child type="lea<caret>f"/>
+                       </example>
+                       """, "<p><b>leaf-Leaf Node</b></p>");
     }
 
     /** 通过在 <code>text</code> 中插入 <code>&lt;caret&gt;</code> 代表光标位置 */
