1ad15b6ab5fa7fb645b58fb4073e3cdf66040f86
canonical <canonical_entropy@163.com>
2025-07-25 22:16:37 +0800

    增加meta orderBy配置测试

---
+++ nop-auth/nop-auth-service/src/test/java/io/nop/auth/service/TestCrudBizModel.java
@@ -0,0 +1,23 @@
+package io.nop.auth.service;
+
+import io.nop.api.core.annotations.autotest.EnableSnapshot;
+import io.nop.api.core.annotations.autotest.NopTestConfig;
+import io.nop.api.core.beans.ApiRequest;
+import io.nop.autotest.junit.JunitAutoTestCase;
+import io.nop.graphql.core.IGraphQLExecutionContext;
+import io.nop.graphql.core.engine.IGraphQLEngine;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+@NopTestConfig(localDb = true,initDatabaseSchema = true)
+public class TestCrudBizModel extends JunitAutoTestCase {
+    @Inject
+    IGraphQLEngine graphQLEngine;
+
+    @EnableSnapshot
+    @Test
+    public void testOrderBy() {
+        IGraphQLExecutionContext ctx = graphQLEngine.newRpcContext(null, "NopAuthOpLog__findPage", ApiRequest.build(null));
+        graphQLEngine.executeRpc(ctx);
+    }
+}
