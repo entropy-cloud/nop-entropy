58664906de9cea9da4ddfdb7ad1baf2303ba9cc8
canonical <canonical_entropy@163.com>
2025-11-04 22:23:32 +0800

    将nop-commons和nop-xdefs模块移动到nop-kernel模块

--- nop-commons/src/main/java/io/nop/commons/collections/IMutableIntArray.java
+++
@@ -1,7 +0,0 @@
-package io.nop.commons.collections;
-
-public interface IMutableIntArray extends IntArray {
-    void set(int index, int value);
-
-    void add(int value);
-}
--- nop-commons/src/main/java/io/nop/commons/text/formatter/ITextFormatter.java
+++
@@ -1,8 +0,0 @@
-package io.nop.commons.text.formatter;
-
-import io.nop.api.core.util.SourceLocation;
-
-public interface ITextFormatter {
-
-    String format(SourceLocation loc, String text, boolean ignoreErrors);
-}
