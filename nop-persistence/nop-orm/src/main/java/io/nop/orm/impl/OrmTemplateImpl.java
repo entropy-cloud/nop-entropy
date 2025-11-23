60b1f703e8b494843ea7d8016438d827941f3f5f
canonical <canonical_entropy@163.com>
2025-11-23 20:20:30 +0800

    还是从session中获取OrmModel，一旦打开session, OrmModel应该固化，否则看到的实体模型不一致可能有微妙的bug

--- nop-persistence/nop-orm/src/main/java/io/nop/orm/impl/OrmTemplateImpl.java
+++ nop-persistence/nop-orm/src/main/java/io/nop/orm/impl/OrmTemplateImpl.java
@@ -78,6 +78,9 @@ public class OrmTemplateImpl extends AbstractSqlExecutor implements IOrmTemplate
 
     @Override
     public IOrmModel getOrmModel() {
+        IOrmSession session = currentSession();
+        if (session != null)
+            return session.getLoadedOrmModel().getOrmModel();
         return sessionFactory.getOrmModel();
     }
 
