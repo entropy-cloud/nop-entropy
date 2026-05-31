8d98385048c3c8a9f9df89c964fdec561067d074
canonical <canonical_entropy@163.com>
2026-05-31 19:06:42 +0800

    feat(auth-web): 落盘action-auth i18n属性与web i18n yaml及precompile2
    - action-auth添加i18n-en:displayName属性
    
    - WebCodeGen添加runPrecompile2调用
    
    - 生成en i18n yaml

--- nop-auth/nop-auth-web/src/test/java/io/nop/auth/web/NopAuthWebCodeGen.java
+++ nop-auth/nop-auth-web/src/test/java/io/nop/auth/web/NopAuthWebCodeGen.java
@@ -28,6 +28,7 @@ public class NopAuthWebCodeGen {
             XCodeGenerator.runPrecompile(new File(projectDir, "../nop-auth-meta"), "/", false);
             XCodeGenerator.runPostcompile(new File(projectDir, "../nop-auth-meta"), "/", false);
             XCodeGenerator.runPrecompile(projectDir, "/", false);
+            XCodeGenerator.runPrecompile2(projectDir, "/", false);
 
         } finally {
             CoreInitialization.destroy();
