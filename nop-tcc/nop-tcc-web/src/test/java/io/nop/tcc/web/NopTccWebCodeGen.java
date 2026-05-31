6c1a14d91922200d41e5f87dc63bea8111134de9
canonical <canonical_entropy@163.com>
2026-05-31 19:07:09 +0800

    feat(tcc-web): 落盘action-auth i18n属性与web i18n yaml及precompile2
    - action-auth添加i18n-en:displayName属性
    
    - WebCodeGen添加runPrecompile2调用
    
    - 生成en i18n yaml

--- nop-tcc/nop-tcc-web/src/test/java/io/nop/tcc/web/NopTccWebCodeGen.java
+++ nop-tcc/nop-tcc-web/src/test/java/io/nop/tcc/web/NopTccWebCodeGen.java
@@ -28,6 +28,7 @@ public class NopTccWebCodeGen {
             XCodeGenerator.runPrecompile(new File(projectDir, "../nop-tcc-service"), "/", false);
             XCodeGenerator.runPostcompile(new File(projectDir, "../nop-tcc-service"), "/", false);
             XCodeGenerator.runPrecompile(projectDir, "/", false);
+            XCodeGenerator.runPrecompile2(projectDir, "/", false);
         } finally {
             CoreInitialization.destroy();
         }
